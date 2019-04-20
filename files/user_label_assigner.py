513c926e58f1b26465b26c072c5789a574fff08b
vrosario <vrosario93@gmail.com>
2019-04-20 18:19:08 -0400

    Update user_label_assigner.py

--- files/user_label_assigner.py
+++ files/user_label_assigner.py
@@ -9,13 +9,18 @@ sh = Shell()
 sh.run('oc get groups')
 for index, oc_groups in enumerate(sh.output()):
     if index > 2:    
-        # print(oc_groups)
-        pp.pprint(oc_groups)
-        group_dn, group_users = oc_groups.split()
+        print(oc_groups)
+        groups_temp = oc_groups.split()
+        group_dn = groups_temp[0]
+        group_users= groups_temp[1:]
         if group_dn == 'CN=ist-students,OU=IST,DC=lab,DC=local':
-            student_users.append(group_users.split(',').strip())
+            for user in group_users:
+                student_users.append(group_users.strip(','))
+            # student_users.append(group_users.split(',').strip())
         elif group_dn == 'CN=ist-faculty,OU=IST,DC=lab,DC=local':
-            faculty_users.append(group_users.split(',').strip())
+            for user in group_users:
+                faculty_users.append(group_users.strip(','))
+            # faculty_users.append(group_users.split(',').strip())
 
 pp.pprint(student_users)
 pp.pprint(faculty_users)
