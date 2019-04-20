8100704168d9ee2900260555968032e30f816c7f
vrosario <vrosario93@gmail.com>
2019-04-20 03:06:20 -0400

    Update user_label_assigner.py

--- files/user_label_assigner.py
+++ files/user_label_assigner.py
@@ -1,6 +1,20 @@
 from shell import Shell
+import pprint
 
+pp = pprint.PrettyPrinter(indent=4)
+student_users = []
+faculty_users = []
 sh = Shell()
 sh.run('oc get groups')
-for file in sh.output():
-    print(file)
+for index, oc_groups in enumerate(sh.output()):
+    if index > 0:    
+        # print(oc_groups)
+        pp.pprint(oc_groups)
+        group_dn, group_users = oc_groups.split()
+        if group_dn == 'CN=ist-students,OU=IST,DC=lab,DC=local':
+            student_users.append(group_users.split(',').strip())
+        elif group_dn == 'CN=ist-faculty,OU=IST,DC=lab,DC=local':
+            faculty_users.append(group_users.split(',').strip())
+
+pp.pprint(student_users)
+pp.pprint(faculty_users)
