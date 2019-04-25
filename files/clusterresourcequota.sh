f75a33c2ae0b904fb39eed4f9f86b979b141f74a
vrosario <vrosario93@gmail.com>
2019-04-25 14:51:55 -0400

    Changed cluster resource quotas
    -Added quota for student-adv level users
    -Changed selector for student-resource-quota

--- files/clusterresourcequota.sh
+++ files/clusterresourcequota.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
  oc create clusterresourcequota student-resource-quota \ 
-    --project-label-selector=level=student \ 
+    --project-label-selector=level=ist-student \ 
     --hard=pods=6 #--hard=secrets=20
+
+ oc create clusterresourcequota student-adv-resource-quota \ 
+    --project-label-selector=level=ist-student-adv \ 
+    --hard=pods= #--hard=secrets=20
