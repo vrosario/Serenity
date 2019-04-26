c17541b09c30f0c1b23daae13946d8845b67c93b
vrosario <vrosario93@gmail.com>
2019-04-26 17:32:38 -0400

    Added core-resource-limits

--- files/clusterresourcequota.sh
+++ files/clusterresourcequota.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#number of pods
  oc create clusterresourcequota student-resource-quota --project-label-selector=level=ist-student --hard=pods=3 #--hard=secrets=20
 
  oc create clusterresourcequota student-adv-resource-quota --project-label-selector=level=ist-student-adv --hard=pods=6 #--hard=secrets=20
+
+
+#create limit range
+oc create -f <limit_range_file> -n <project>
+oc create -f pod-limit-range.yaml -n prusso-test-1
