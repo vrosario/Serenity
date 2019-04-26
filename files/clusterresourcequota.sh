62ad0e69c13fa3f1fb054eb83ce0d2ad5eb7794e
vrosario <vrosario93@gmail.com>
2019-04-26 14:25:11 -0400

    Changed default pod limit to 3

--- files/clusterresourcequota.sh
+++ files/clusterresourcequota.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
- oc create clusterresourcequota student-resource-quota \ 
-    --project-label-selector=level=ist-student \ 
-    --hard=pods=6 #--hard=secrets=20
+ oc create clusterresourcequota student-resource-quota --project-label-selector=level=ist-student --hard=pods=3 #--hard=secrets=20
 
- oc create clusterresourcequota student-adv-resource-quota \ 
-    --project-label-selector=level=ist-student-adv \ 
-    --hard=pods= #--hard=secrets=20
+ oc create clusterresourcequota student-adv-resource-quota --project-label-selector=level=ist-student-adv --hard=pods=6 #--hard=secrets=20
